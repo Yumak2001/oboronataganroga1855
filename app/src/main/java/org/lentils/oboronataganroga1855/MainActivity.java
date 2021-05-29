7cb14a8c7bbd755a242b469b28f09d8fdc3c8cbb
Nikita <nik.safronov.2014@bk.ru>
2021-05-29 18:05:11 +0300

    ADD registr AND sign AND accountActivity

--- app/src/main/java/org/lentils/oboronataganroga1855/MainActivity.java
+++ app/src/main/java/org/lentils/oboronataganroga1855/MainActivity.java
@@ -1,14 +1,74 @@
 package org.lentils.oboronataganroga1855;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.lentils.oboronataganroga1855.register.ActivityRegistr;
+
+import java.sql.DatabaseMetaData;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    private String userID;
+
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logout = (Button) findViewById(R.id.signOut);
+
+        logout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, ActivityRegistr.class));
+            }
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView fullNameTextView = (TextView) findViewById(R.id.textFullname);
+        final TextView emailTextView = (TextView) findViewById(R.id.textEmail);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if (userProfile != null){
+                    String fullName = userProfile.fullName;
+                    String email = userProfile.email;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, "Something wrong happened!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
